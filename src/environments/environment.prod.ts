989b46d8cc7aa2958ee3cc89905cdaa71d45de18
Ilhan Basic <ilhanbasic456@gmail.com>
2026-03-09 12:28:13 +0100

    chore: remove environment configuration files

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://pausalio-cgbtcbbwbze0d2bb.germanywestcentral-01.azurewebsites.net/api',
-    hubUrl: 'https://pausalio-cgbtcbbwbze0d2bb.germanywestcentral-01.azurewebsites.net' 
-  };
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'https://pausalio-cgbtcbbwbze0d2bb.germanywestcentral-01.azurewebsites.net/api',
-    hubUrl: 'https://pausalio-cgbtcbbwbze0d2bb.germanywestcentral-01.azurewebsites.net' 
-    // apiUrl: 'https://localhost:7272/api',
-    // hubUrl: 'https://localhost:7272' 
-  };
